cb8329602e139c256c34212bc924a6d84bb4eb97
VictorNwobodo <anthonyvictoran2003@gmail.com>
2023-11-21 21:51:59 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+// We have two types of datatypes
+
+// Primitive datatypes below are examples of primitive datatypes.
+
+// String 
+"Hello Victor";
+
+// Numbers or float
+2.2, 3.4, 10.000000, 40
+
+//Boleans
+true // used when statement is true.
+false // used when a statement is not true.
+
+//undefined
+x // the x values is not defined cause no value was assigned to it for execution.
+
+// null
+
+//isNaN
+
+
+// then we have non-primitive datatypes
+
+// arrays and Objects
---
+++ variables.js
@@ -0,0 +1,22 @@
+// Variables are used to assign value. eg.
+
+var age = 20
+var month = 'November'
+var year = 2003
+var qualification = 'High School Certificate'
+
+identity = name
+name = 'Victoran2003'
+
+console.log(age)
+console.log(month)
+console.log(year)
+console.log(qualification)
+console.log(name)
+// rules of creating a Variables
+
+//1. your variable name must not start with a number or special character.
+
+//2. They must be not space between the variable name.
+
+//3. You can seperate your variable name with an underscore "_" or "-";
